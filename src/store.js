d7e1bd6083f51157b4733a464c9c84ecde450302
Sean Bunch <databaws@gmail.com>
2023-08-23 20:12:14 -0400

    extra files deleted from redux update

--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import popupSlice from "./components/popupModalSlice";
 import circleTimerSlice from "./components/circleTimerSlice";
 
 export const store = configureStore({
