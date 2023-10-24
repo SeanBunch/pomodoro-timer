3432e2a0b71ac43422a33d5c0f7159f82024e85e
Sean Bunch <databaws@gmail.com>
2023-10-24 18:57:25 -0400

    express-sessons with knex added, cookie time!

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,15 +7,37 @@ const loginRouter = require("./login/login.router");
 const cors = require("cors");
 
 const app = express();
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+const knex = require("./db/connection");
+const store = new KnexSessionStore({
+  knex,
+  tablename: "sessions",
+});
 
 app.use(cors());
 app.use(express.json());
+
+app.use(
+  session({
+    secret: "notell",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 10000,
+    },
+  store,
+  })
+);
 
 app.use("/tasklist", taskListRouter);
 app.use("/login", loginRouter);
 
 app.use("/", (req, res, next) => {
-  res.json({ data: "pomotime server online" });
+  const n = req.session.views || 1;
+  req.session.views = n + 1;
+  res.json({ data: `pomotime server online with ${n} views` });
+  // res.end(`${n} views`);
 });
 
 // not found handler
